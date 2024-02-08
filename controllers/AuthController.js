11ddc609fd38f11a7b25c09f2e0c1687253bb92f
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2024-02-08 20:11:18 +0100

    file manager

---
+++ controllers/AuthController.js
@@ -0,0 +1,68 @@
+const redisClient = require('../utils/redis').default;
+const dbClient = require('../utils/db').default;
+const sha1 = require('sha1');
+const { v4 } = require('uuid');
+
+const getConnect = async (req, res) => {
+    const Authorization = req.header('Authorization');
+    if (!Authorization) {
+        return res.status(401).json({
+            error: 'Unauthorized'
+        });
+    }
+    const base64Credentials = Authorization.split(' ')[1];
+    const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
+    const email = credentials.split(':')[0];
+    console.log(email)
+    const password = credentials.split(':')[1];
+    const hashpass = sha1(password);
+    console.log(hashpass)
+    try {
+        const findUser = await dbClient.db.collection('users').findOne({ email: email, password: hashpass });
+        if (!findUser) {
+            return res.status(401).json({
+                error: 'Unauthorized'
+            });
+        } else {
+
+            const token = v4();
+            const key = `auth_${token}`;
+            redisClient.set(key, findUser._id.toString(), 86400);
+            return res.status(200).json({
+                token: token
+            });
+        }
+    } catch (err) {
+        return res.status(500).json({
+            error: err.message
+        });
+    }
+};
+
+// logout
+const getDisconnect = async (req, res) => {
+    const token = req.header('X-Token');
+    if (!token) {
+        return res.status(401).json({
+            error: 'Unauthorized'
+        });
+    }
+    try {
+        const userId = await redisClient.get(`auth_${token}`);
+        console.log(userId);
+        if (!userId) {
+            return res.status(401).json({
+                error: 'Unauthorized'
+            });
+        }
+        await redisClient.del(`auth_${token}`);
+        return res.status(204).json();
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            error: err.message
+        });
+    }
+};
+
+module.exports = { getConnect, getDisconnect }
---
+++ utils/db.js
@@ -0,0 +1,35 @@
+import { MongoClient } from 'mongodb';
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+
+class DBClient {
+  constructor() {
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (!err) {
+        this.db = client.db(DB_DATABASE);
+      } else {
+        console.error('Error connecting to MongoDB:', err);
+        this.db = false;
+      }
+    });
+  }
+
+  isAlive() {
+    return this.db? true : false;
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
